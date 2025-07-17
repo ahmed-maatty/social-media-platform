df11906fa1780c743e183edb4a2402453faffdd8
ahmed Maatty <99508250+ahmed-maatty@users.noreply.github.com>
2025-07-18 02:55:56 +0300

    Update domain.js

--- client/src/api/domain.js
+++ client/src/api/domain.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Domain = axios.create({
-    baseURL:"http://localhost:5000"
+    baseURL:"https://social-media-platform-rho.vercel.app"
 })
 
 export default Domain ;
